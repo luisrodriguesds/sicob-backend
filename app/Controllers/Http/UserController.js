f27ba805aaa0b3937182259b08d52afb17f84603
ctsicob <ctsicob@ufc.br>
2019-07-13 12:24:35 -0300

    Revert "Alterando novamente para email e senha"
    This reverts commit b52a689885774f028e0b4dd1afd06f92390e2891.

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -132,8 +132,8 @@ class UserController {
     }
 
     async authentication({request, auth}){
-        const {email, password} = request.all();
-        const token = await auth.attempt(email, password);
+        const {username, password} = request.all();
+        const token = await auth.attempt(username, password);
         return token;
     }
 
